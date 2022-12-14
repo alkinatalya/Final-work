311601bfbe88995157d0774957357b802010965a
Natalya <alkinatalya@yandex.com>
2022-12-14 13:55:09 +0300

    add сама программа

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,30 @@
 ﻿Console.WriteLine("Введите значения через пробел: ");
 string[] array = Console.ReadLine()!.Split(" ");
+
+string[] result = FindLessThan(array, 3);
+
+string[] FindLessThan(string[] input, int n) 
+{
+    string[] output = new string[CountLessThan(input, n)];
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+int CountLessThan(string[] input, int n) 
+{
+    int count = 0;
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
